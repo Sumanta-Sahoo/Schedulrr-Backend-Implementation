d801fa605a4673081b257923f7c48ec26d3fb026
[Sumanta Sahoo] <sumantasahoo138@gmail.com>
2024-09-26 10:35:51 +0530

    Setting up springboot project folder for end-point API development

---
+++ src/main/java/com/schedulrr/SchedulrrApplication.java
@@ -0,0 +1,13 @@
+package com.schedulrr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulrrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulrrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schedulrr/SchedulrrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schedulrr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulrrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
