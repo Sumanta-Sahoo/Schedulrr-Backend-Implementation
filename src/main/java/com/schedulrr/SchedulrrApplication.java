ed746cbf835c2508623116eb511ff4d37cb37e76
[Sumanta Sahoo] <sumantasahoo138@gmail.com>
2024-09-30 19:18:32 +0530

    feat: Add ModelMapper library to pom.xml and configure in SchedulrrApplication.java
    - Added ModelMapper dependency to pom.xml for easy object mapping.
    - Configured ModelMapper bean in Application.java for use across the application.

--- src/main/java/com/schedulrr/SchedulrrApplication.java
+++ src/main/java/com/schedulrr/SchedulrrApplication.java
@@ -1,7 +1,9 @@
 package com.schedulrr;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -11,4 +13,8 @@ public class SchedulrrApplication {
 		SpringApplication.run(SchedulrrApplication.class, args);
 	}
 
+	@Bean
+	public static ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 }
